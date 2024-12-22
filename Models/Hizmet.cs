080aa52e4618249c3fc2b4e0f69793e8c9c4c131
abdullah bünyamin adsever <126345264+bunyamin60@users.noreply.github.com>
2024-12-22 22:53:30 +0300

    Update Hizmet.cs
    hizmet model degisim

--- Models/Hizmet.cs
+++ Models/Hizmet.cs
@@ -5,13 +5,17 @@ namespace WEBBERBERODEV.Models
     public class Hizmet
     {
         public int Id { get; set; }
-        public int SalonId { get; set; }
+        
         [Required]
         public string Ad { get; set; }
+        [Required]
+        [Range(1,480, ErrorMessage ="SureDakika 1 ile 480 arasında olmalıdır.")]
         public int SureDakika { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
         public decimal Fiyat { get; set; }
 
-        public Salon Salon { get; set; }
-        public ICollection<CalisanHizmet> CalisanHizmetler { get; set; }
+        
+        public ICollection<CalisanHizmet> CalisanHizmetler { get; set; } = new List<CalisanHizmet>();
     }
 }
