27f7a74756443473a1f75116a0778615ce4c311a
abdullah bünyamin adsever <126345264+bunyamin60@users.noreply.github.com>
2024-12-25 19:16:12 +0300

    yeni model eklenmesi

---
+++ Models/CalisanCalismaSaatleri.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WEBBERBERODEV.Models
+{
+    public class CalisanCalismaSaatleri
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int CalisanId { get; set; }
+        public Calisan Calisan { get; set; }
+
+        [Required]
+        public DayOfWeek Gun { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
+        public TimeSpan BaslangicSaati { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
+        public TimeSpan BitisSaati { get; set; }
+    }
+}
---
+++ Models/CalisanCreateViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WEBBERBERODEV.Models
+{
+    public class CalisanCreateViewModel
+    {
+        // Identity tarafında lazım olan bilgiler
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        // Calisan tablosu için
+        [Required]
+        public string Ad { get; set; }
+
+        [Required]
+        public string Soyad { get; set; }
+
+        [Required]
+        public string Uzmanlik { get; set; }
+
+        public bool AktifMi { get; set; }
+    }
+}
