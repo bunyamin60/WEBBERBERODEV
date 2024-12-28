5069d9c1770fcc552973f551343c93d1f7d89318
abdullah bünyamin adsever <126345264+bunyamin60@users.noreply.github.com>
2024-12-29 00:08:49 +0300

    Add files via upload

---
+++ Models/RandevuHizmet.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WEBBERBERODEV.Models
+{
+    public class RandevuHizmet
+    {
+        public int RandevuId { get; set; }
+        public Randevu Randevu { get; set; }
+
+        public int HizmetId { get; set; }
+        public Hizmet Hizmet { get; set; }
+    }
+}
---
+++ Models/RandevuViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WEBBERBERODEV.Models
+{
+    public class RandevuViewModel
+    {
+        public int? Id { get; set; }
+
+        [Required]
+        [Display(Name = "Çalışan")]
+        public int CalisanId { get; set; }
+
+        [Required]
+        [Display(Name = "Randevu Tarihi ve Saati")]
+        [DataType(DataType.DateTime)]
+        [FutureDate(ErrorMessage = "Randevu tarihi gelecekte olmalıdır.")]
+        public DateTime RandevuTarihi { get; set; }
+
+        [Required]
+        [Display(Name = "Seçilen Hizmetler")]
+        public List<int> HizmetIds { get; set; } = new List<int>();
+    }
+
+    // Gelecek tarihler için doğrulama attribute'u
+    public class FutureDateAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is DateTime dateTime)
+            {
+                return dateTime > DateTime.Now;
+            }
+            return false;
+        }
+    }
+}
