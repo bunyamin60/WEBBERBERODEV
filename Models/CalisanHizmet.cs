f7c3262e1309a67e0ab7cf6e31ca46307a90c437
abdullah bünyamin adsever <126345264+bunyamin60@users.noreply.github.com>
2024-12-29 00:07:15 +0300

    Update CalisanHizmet.cs

--- Models/CalisanHizmet.cs
+++ Models/CalisanHizmet.cs
@@ -1,12 +1,16 @@
-﻿namespace WEBBERBERODEV.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WEBBERBERODEV.Models
 {
     public class CalisanHizmet
     {
-        public int CalisanId { get; set; }
-        public int HizmetId { get; set; }
 
-        public Calisan Calisan { get; set; }
-        public Hizmet Hizmet { get; set; }
-    
+        [Key, Column(Order = 0)]
+        public int CalisanId { get; set; }
+        public Calisan? Calisan { get; set; }
+        [Key, Column(Order = 1)]
+        public int HizmetId { get; set; }
+        public Hizmet? Hizmet { get; set; }
     }
 }
