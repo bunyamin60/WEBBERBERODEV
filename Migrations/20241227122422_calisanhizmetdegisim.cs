48ea770a25f576a0fcc823517882ba4c179755eb
abdullah bünyamin adsever <126345264+bunyamin60@users.noreply.github.com>
2024-12-29 00:05:32 +0300

    Add files via upload

---
+++ Migrations/20241227122422_calisanhizmetdegisim.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WEBBERBERODEV.Migrations
+{
+    /// <inheritdoc />
+    public partial class calisanhizmetdegisim : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "HizmetId",
+                table: "CalisanHizmetler",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("Relational:ColumnOrder", 1);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CalisanId",
+                table: "CalisanHizmetler",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("Relational:ColumnOrder", 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "HizmetId",
+                table: "CalisanHizmetler",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("Relational:ColumnOrder", 1);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CalisanId",
+                table: "CalisanHizmetler",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("Relational:ColumnOrder", 0);
+        }
+    }
+}
---
+++ Migrations/20241227131104_uPdataCalisanHizmetModel.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WEBBERBERODEV.Migrations
+{
+    /// <inheritdoc />
+    public partial class uPdataCalisanHizmetModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CalisanHizmetler_Hizmetler_HizmetId1",
+                table: "CalisanHizmetler");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CalisanHizmetler_HizmetId1",
+                table: "CalisanHizmetler");
+
+            migrationBuilder.DropColumn(
+                name: "HizmetId1",
+                table: "CalisanHizmetler");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "HizmetId",
+                table: "CalisanHizmetler",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("Relational:ColumnOrder", 1);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CalisanId",
+                table: "CalisanHizmetler",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("Relational:ColumnOrder", 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "HizmetId",
+                table: "CalisanHizmetler",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("Relational:ColumnOrder", 1);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CalisanId",
+                table: "CalisanHizmetler",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("Relational:ColumnOrder", 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "HizmetId1",
+                table: "CalisanHizmetler",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CalisanHizmetler_HizmetId1",
+                table: "CalisanHizmetler",
+                column: "HizmetId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CalisanHizmetler_Hizmetler_HizmetId1",
+                table: "CalisanHizmetler",
+                column: "HizmetId1",
+                principalTable: "Hizmetler",
+                principalColumn: "Id");
+        }
+    }
+}
