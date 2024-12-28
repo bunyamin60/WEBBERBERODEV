a103a94328c2c468d9d11aa91dbed84143f67585
abdullah bünyamin adsever <126345264+bunyamin60@users.noreply.github.com>
2024-12-29 00:03:25 +0300

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,37 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WEBBERBERODEV.DATA;
 using WEBBERBERODEV.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 
 namespace WEBBERBERODEV.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var viewModel = new HomePageViewModel
+            {
+                SalonAdi = "B�N BERBER",
+                AcilisKapanisSaati = "10:00 - 20:00",
+                Hizmetler = await _context.Hizmetler.ToListAsync(),
+                Calisanlar = await _context.Calisanlar
+                                    .Where(c => c.AktifMi)
+                                    .Include(c => c.ApplicationUser)
+                                    .ToListAsync()
+            };
+
+            return View(viewModel);
         }
 
         public IActionResult Privacy()
