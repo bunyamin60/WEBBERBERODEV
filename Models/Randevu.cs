61499e8cf097a2ba5e51581e77c8d7f76f463d9e
abdullah bünyamin adsever <126345264+bunyamin60@users.noreply.github.com>
2024-12-29 00:08:28 +0300

    Update Randevu.cs

--- Models/Randevu.cs
+++ Models/Randevu.cs
@@ -1,22 +1,38 @@
-﻿namespace WEBBERBERODEV.Models
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WEBBERBERODEV.Models
 {
     public class Randevu
     {
         public int Id { get; set; }
-        public string UserId { get; set; } // AspNetUsers tablosundaki kullanıcının Id'si
-        public int CalisanId { get; set; }
-        public int HizmetId { get; set; }
-        public DateTime TarihSaat { get; set; }
-        public decimal Fiyat { get; set; }
-        public string Durum { get; set; }
 
-        // Navigation properties
+        [Required]
+        public int CalisanId { get; set; }
         public Calisan Calisan { get; set; }
-        public Hizmet Hizmet { get; set; }
-        // User bilgisi için direkt AspNetUsers tablosuna gidilir.
-        // Burada AspNetUsers ile doğrudan bir navigation yapmak istersen 
-        // IdentityUser tipini eklemek ve Identity entegre etmek gerekir.
-        // Şimdilik basit kalsın.
+
+        public ICollection<RandevuHizmet> RandevuHizmetler { get; set; } = new List<RandevuHizmet>();
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime RandevuTarihi { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Fiyat { get; set; }
+
+        [Required]
+        public RandevuDurumu Durum { get; set; }
+
+        [Required]
+        public string KullaniciId { get; set; }
+        public ApplicationUser Kullanici { get; set; }
     }
 
+    public enum RandevuDurumu
+    {
+        Beklemede,
+        Onaylandi,
+        IptalEdildi
+    }
 }
