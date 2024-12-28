66f368da5158edf211450d721c2ab55f6cdd1611
abdullah bünyamin adsever <126345264+bunyamin60@users.noreply.github.com>
2024-12-29 00:07:29 +0300

    Add files via upload

---
+++ Models/EmployeeDailyEarningsViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WEBBERBERODEV.Models
+{
+    public class EmployeeDailyEarningsViewModel
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public DateTime Date { get; set; }
+        public decimal TotalEarnings { get; set; }
+        public int TotalAppointments { get; set; }
+    }
+}
