2d836a564048ba42154caf76f1a29888323be10a
abdullah bünyamin adsever <126345264+bunyamin60@users.noreply.github.com>
2024-12-29 00:08:07 +0300

    Add files via upload

---
+++ Models/HomePageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace WEBBERBERODEV.Models
+{
+    public class HomePageViewModel
+    {
+        public string SalonAdi { get; set; }
+        public string AcilisKapanisSaati { get; set; }
+        public List<Hizmet> Hizmetler { get; set; }
+        public List<Calisan> Calisanlar { get; set; }
+    }
+}
