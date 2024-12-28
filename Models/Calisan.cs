ff9599700fd3c05ce60b08fa8ea0967bf6accb30
abdullah bünyamin adsever <126345264+bunyamin60@users.noreply.github.com>
2024-12-29 00:06:03 +0300

    Update Calisan.cs

--- Models/Calisan.cs
+++ Models/Calisan.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WEBBERBERODEV.Models
 {
@@ -14,7 +15,14 @@ namespace WEBBERBERODEV.Models
         public string Uzmanlik { get; set; }
         public bool AktifMi { get; set; }
 
-       
+        // Çalışan Identity tablosundaki hangi kullanıcıya karşılık geliyor?
+        public string? ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
+        [NotMapped]
+        public string AdSoyad => $"{Ad} {Soyad}";
+        // Çalışanın çalışma saatlerini temsil eden koleksiyon
+        public ICollection<CalisanCalismaSaatleri> CalisanCalismaSaatleri { get; set; } = new List<CalisanCalismaSaatleri>();
         public ICollection<CalisanHizmet> CalisanHizmetler { get; set; } = new List<CalisanHizmet>();
     }
 }
