0adce4f76e3d29e0de0b19cdbae2bcf71af7d0a4
abdullah bünyamin adsever <126345264+bunyamin60@users.noreply.github.com>
2024-12-29 00:03:39 +0300

    Add files via upload

---
+++ Controllers/PerformanceController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WEBBERBERODEV.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace WEBBERBERODEV.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class PerformanceController : Controller
+    {
+        private readonly EmployeeDailyEarningsService _earningsService;
+
+        public PerformanceController(EmployeeDailyEarningsService earningsService)
+        {
+            _earningsService = earningsService;
+        }
+
+        // GET: Performance/DailyEarnings
+        public async Task<IActionResult> DailyEarnings(DateTime? date)
+        {
+            var selectedDate = date ?? DateTime.Today;
+            var earnings = await _earningsService.GetDailyEarningsAsync(selectedDate);
+            ViewBag.SelectedDate = selectedDate;
+            return View(earnings);
+        }
+    }
+}
